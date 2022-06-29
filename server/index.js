0e9dd618d71a093e5c4f3e4106e3219c0b7e1e23
Clara Dehlin <claramargaretdehlin@gmail.com>
2022-06-29 12:32:46 -0600

    ads

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ app.get('/css', (req, res) => {
 })
 
 app.get('/js', (req, res) => {
-    res.sendFile(path.join(__dirname, '../styles.js'))
+    res.sendFile(path.join(__dirname, '../index.js'))
 })
 
 const port = process.env.PORT || 4005
